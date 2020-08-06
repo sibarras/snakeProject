b3364b1aaa61d17736d06d90d0b8ad59c4e213df
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-08-06 01:51:26 -0500

    manejando keylogger

--- inputreader.py
+++ inputreader.py
@@ -1,15 +1,48 @@
 #!/usr/bin/python3
-from pynput.keyboard import Key, Listener 
+from pynput.keyboard import Key, Listener
 
-pressed = []
-def on_press(key):
-    pressed.append(key)
-    print("{}".format(key))
+# vamos a utilizar la condicion de tener o no presionada una tecla para
+# usarlo como on off del keylogger y cada vez que la serpiente se mueva lo
+# volvemos a activar
+class Keylogger:
+    pressed = None
+    end = False
+    running = False
 
-def on_release(key):
-    if key == Key.esc:
-        print(pressed)
-        return False
+    @classmethod
+    def on_press(cls, key):
+        key = str(key).lstrip('Key.')
+        if key in ['up','down','left','right']:
+            cls.stop()
+            cls.pressed = key
+            print('finish '+key)
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+    @classmethod
+    def on_release(cls, key):
+        if cls.end == True:
+            cls.running = False
+            return False
+
+    @classmethod
+    def start(cls):
+        cls.pressed = None
+        cls.running = True
+        cls.end = False
+        with Listener(on_press=cls.on_press, on_release=cls.on_release) as listener:
+            listener.join()
+
+
+    @classmethod
+    def stop(cls):
+        cls.end = True
+
+from time import time, sleep
+ti = time()
+while time()-ti < 10:
+    sleep(1)
+    print(time()-ti)
+    if not Keylogger.running:
+        Keylogger.start()
+    if Keylogger.pressed != None:
+        break
+Keylogger.stop()
